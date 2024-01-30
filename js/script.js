e1a059bec8d14540183e098df052d8eecee160a4
arianna-cosimini <arianna.cosimini@gmail.com>
2024-01-30 21:00:47 +0100

    Milestone 2 completato

--- js/script.js
+++ js/script.js
@@ -1,35 +1,26 @@
 
-
-
-
 const myButton = document.querySelector(".btn");
 myButton.addEventListener("click",
     function () {
         let nomeCognome = document.getElementById("inputName").value;
+        let namePassenger = document.getElementById("name")
         let kmDaPercorrere = document.getElementById("inputKm").value;
         let ageSelect = document.getElementById("age-select");
+        let costTicket =  document.getElementById("price-ticket");
+        let typeOfTicket= document.getElementById("type-ticket");
         let choiceUser = ageSelect.value;
-        
-        // ageSelect.addEventListener("change", 
-        //     function(){
-        //         const choiceUser = ageSelect.value;
-
-        //         console.log("scelta dell'utente:", choiceUser);
-        //     }
-        // )
-
 
         const priceKm = 0.21;
 
-        const priceTicket = kmDaPercorrere * 0.21;
-        console.log('Il prezzo per gli utenti di età compresa tra 18 e 65 è:' + priceTicket);
+        let priceTicket = kmDaPercorrere * 0.21;
+        // console.log('Il prezzo per gli utenti di età compresa tra 18 e 65 è:' + priceTicket);
 
         // prezzo minorenni
 
         const percentageMinors = 20;
         const discountMinors = ((percentageMinors / 100) * priceTicket);
         const priceMinors = (priceTicket - discountMinors).toFixed(2);
-        console.log('Il prezzo scontato per minoreenni è:€' + priceMinors);
+        // console.log('Il prezzo scontato per minoreenni è:€' + priceMinors);
 
         // fine prezzo minorenni
 
@@ -38,18 +29,35 @@ myButton.addEventListener("click",
         const percentageOver65 = 40;
         const discountOver65 = ((percentageOver65 / 100) * priceTicket);
         const priceOver65 = (priceTicket - discountOver65).toFixed(2);
-        console.log('Il prezzo scontato per over 65 è:€' + priceOver65);
+        // console.log('Il prezzo scontato per over 65 è:€' + priceOver65);
 
 
         // fine prezzo over 65
 
         if (choiceUser==1) {
-            console.log("L'utente ha un'età compresa tra 18 e 65 anni")
+            namePassenger.innerHTML= (nomeCognome);
+            console.log("Nome passeggero")
+            costTicket.innerHTML="€ " + priceTicket;
+            typeOfTicket.innerHTML="Biglietto standard"
+            console.log("Prezzo utente tra 18 e 65 anni: € " + priceTicket)
+            
         } else if (choiceUser==2) {
-            console.log("L'utente è minorenne")
+            namePassenger.innerHTML= (nomeCognome);
+            console.log("Nome passeggero")
+            costTicket.innerHTML="€ " + priceMinors;
+            typeOfTicket.innerHTML="Biglietto scontato del 20%"
+
+            console.log("Prezzo utente minorenne: € " + priceMinors)
+           
 
         } else if (choiceUser==3) {
-            console.log("L'utente ha più di 65 anni")
+            namePassenger.innerHTML= (nomeCognome);
+            console.log("Nome passeggero")
+            costTicket.innerHTML="€ " + priceOver65;
+            typeOfTicket.innerHTML="Biglietto scontato del 40%"
+
+            console.log("Prezzo utente over 65: € " + priceOver65)
+            
 
         }
     })
